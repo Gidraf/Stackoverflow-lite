b40afb3a1fc22b11d9786474f011ac8f4d975c9f
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-14 14:23:06 +0300

    [feature #159717813] create question get views endpoint

---
+++ app/questions/__init__.py
@@ -0,0 +1,27 @@
+"""initialize question blueprint"""
+from flask import Blueprint
+
+QUESTION=Blueprint("questions", __name__)
+question_list=[{
+    "id": 1,
+    "title": "this is my first question?",
+    "description": "this iis my first description",
+    "answer": 0,
+    "time": "11:27 am"}, {
+        "id": 1,
+        "title": "this is my second question?",
+        "description": "this iis my second description",
+        "answer": 1,
+        "time": "11:50 am"}, {
+            "id": 1,
+            "title": "this is my third question?",
+            "description": "this iis my third description",
+            "answer": 2,
+            "time": "11:50 am"}, {
+                "id": 1,
+                "title": "this is my fourth question?",
+                "description": "this iis my fourth description",
+                "answer": 3,
+                "time": "11:30 am"}]
+
+from . import questions_views
---
+++ app/questions/questions_views.py
@@ -0,0 +1,22 @@
+'''this module is intented to do all the routing in question api'''
+from flask import request
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from app.models.models import Question
+from . import QUESTION
+from . import question_list
+
+question=Question(question_list)
+
+@QUESTION.errorhandler(404)
+def not_found(error):
+    """customed error handler"""
+    return make_response(jsonify({"error":"no item found"}),404)
+
+@QUESTION.route("/api/v1/questions")
+def home():
+    """show all questions"""
+    if question:
+        return jsonify({"questions":question.show_questions()})
+    abort(404)
