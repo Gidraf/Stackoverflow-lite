0e6093caa6cb034778e4a20693d7287b44f7399a
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-14 14:30:31 +0300

    [fixing #159717813] making module more readable

--- app/questions/questions_views.py
+++ app/questions/questions_views.py
@@ -7,16 +7,16 @@ from app.models.models import Question
 from . import QUESTION
 from . import question_list
 
-question=Question(question_list)
+question = Question(question_list)
 
 @QUESTION.errorhandler(404)
 def not_found(error):
     """customed error handler"""
-    return make_response(jsonify({"error":"no item found"}),404)
+    return make_response(jsonify({"error": "no item found"}),404)
 
 @QUESTION.route("/api/v1/questions")
 def home():
     """show all questions"""
     if question:
-        return jsonify({"questions":question.show_questions()})
+        return jsonify({"questions": question.show_questions()})
     abort(404)
