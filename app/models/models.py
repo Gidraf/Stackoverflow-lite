689fb9ca078939c2a42d4a9f3da065c8edacf2dc
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-15 22:41:27 +0300

    fix implimenting OOP

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,5 @@
 """this module it contains all the models"""
+from flask import abort
 
 class Question(object):
     """question model"""
@@ -14,10 +15,27 @@ class Question(object):
         """add questions to the list"""
         self.question_list.append(question)
 
-    def update_questions(self, index, question):
+    def update_questions(self, index, title, description, time):
         "update a question"
-        self.question_list[index] = question
+        new_question = [new_question for new_question in self.question_list if new_question["id"] == index]
+        if new_question:
+            new_question[0]["title"] = title
+            new_question[0]["description"] = description
+            new_question[0]["time"] = time
+            return new_question[0]
 
     def delete_question(self, index):
         """delete questions"""
-        del self.question_list[index]
+        del_question = [del_question for del_question in self.question_list if del_question["id"] == index]
+        if del_question:
+            self.question_list.remove(del_question[0])
+            return True
+        return False
+
+
+class Answer(object):
+    """questions answers"""
+
+    def  __init__(self, answer_list):
+        """initialized question list"""
+        self.answer_list = answer_list
