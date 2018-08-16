ea8d4751703ceac3899b009f1d60e225d8b1327d
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-16 17:48:14 +0300

    [feature #159717827] create answer list

--- app/questions/__init__.py
+++ app/questions/__init__.py
@@ -6,7 +6,6 @@ question_list=[{
     "id": 1,
     "title": "this is my first question?",
     "description": "this iis my first description",
-    "answer": 0,
     "time": "11:27 am"}, {
         "id": 2,
         "title": "this is my second question?",
@@ -23,5 +22,98 @@ question_list=[{
                 "description": "this iis my fourth description",
                 "answer": 3,
                 "time": "11:30 am"}]
+
+answer_list = [{
+    "id": 1,
+    "answer_text": " this is the first answer for this answer",
+    "time": " 11:00 am",
+    "votes":0,
+    "question_id": 1
+    },{
+        "id": 2,
+        "answer_text": " this is the second answer for this answer",
+        "time": " 11:00 am",
+        "votes":0,
+        "question_id": 1
+        },{
+            "id": 3,
+            "answer_text": " this is the first answer for this answer",
+            "time": " 11:00 am",
+            "votes":0,
+            "question_id": 2
+            },{
+                "id": 4,
+                "answer_text": " this is the second answer for this answer",
+                "time": " 11:00 am",
+                "votes":0,
+                "question_id": 2
+                },{
+                    "id": 5,
+                    "answer_text": " this is the first answer for this answer",
+                    "time": " 11:00 am",
+                    "votes":0,
+                    "question_id": 3
+                    },{
+                        "id": 6,
+                        "answer_text": " this is the second answer for this answer",
+                        "time": " 11:00 am",
+                        "votes":0,
+                        "question_id": 3
+                        },{
+                            "id": 7,
+                            "answer_text": " this is the first answer for this answer",
+                            "time": " 11:00 am",
+                            "votes":0,
+                            "question_id": 4
+                            },{
+                                "id": 8,
+                                "answer_text": " this is the second answer for this answer",
+                                "time": " 11:00 am",
+                                "votes":0,
+                                "question_id": 4
+                                },{
+                                    "id": 9,
+                                    "answer_text": " this is the third answer for this answer",
+                                    "time": " 11:00 am",
+                                    "votes":0,
+                                    "question_id": 1
+                                    },{
+                                        "id": 10,
+                                        "answer_text": " this is the fourth answer for this answer",
+                                        "time": " 11:00 am",
+                                        "votes":0,
+                                        "question_id": 1
+                                        },{
+                                            "id": 11,
+                                            "answer_text": " this is the third answer for this answer",
+                                            "time": " 11:00 am",
+                                            "votes":0,
+                                            "question_id": 2
+                                            },{
+                                                "id": 12,
+                                                "answer_text": " this is the fourth answer for this answer",
+                                                "time": " 11:00 am",
+                                                "votes":0,
+                                                "question_id": 2
+                                                },{
+                                                    "id": 13,
+                                                    "answer_text": " this is the third answer for this answer",
+                                                    "time": " 11:00 am",
+                                                    "votes":0,
+                                                    "question_id": 3
+                                                    },{
+                                                        "id": 14,
+                                                        "answer_text": " this is the fourth answer for this answer",
+                                                        "time": " 11:00 am",
+                                                        "votes":0,
+                                                        "question_id": 3
+                                                        },{
+                                                            "id": 15,
+                                                            "answer_text": " this is the third answer for this answer",
+                                                            "time": " 11:00 am",
+                                                            "votes":0,
+                                                            "question_id": 4
+                                                            },]
+
 
 from . import questions_views
