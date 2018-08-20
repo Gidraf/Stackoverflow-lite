6afb700ced426cefb856f08414aeed46a6faf6ee
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-21 00:45:18 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -13,12 +13,11 @@ setup(
         'Intended Audience :: All',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
      packages=find_packages()
 )
