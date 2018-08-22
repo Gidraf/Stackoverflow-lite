c097cd9ab80cf336de8c4b6a46f1f6d3e89a1e0a
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-22 17:25:35 +0300

    [finishes #159870393] setting database

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,9 @@
+import psycopg2
+from instance.config import params
 
+
+def database_connection():
+    """connect to database"""
+    connection=psycopg2.connect(**params)
+
+    return connection
