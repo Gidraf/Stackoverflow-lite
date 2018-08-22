dbc68c974f18e37bb7593b46ae163d41853f47cb
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-22 17:30:55 +0300

    rergistering user endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 """parent file for the app"""
 from flask import Flask
-from .questions import QUESTION
+from .resources.questions import QUESTION
+from .resources.users import users
 from instance.config import app_config
 
 
@@ -9,6 +10,7 @@ def create_app(config_name):
     """create APP"""
     APP=Flask(__name__, instance_relative_config=True)
     APP.register_blueprint(QUESTION)
+    APP.register_blueprint(users)
     APP.config.from_object(app_config[config_name])
     APP.config.from_pyfile('config.py')
     return APP
