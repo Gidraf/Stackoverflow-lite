a32b0a24dc53f2ef82769b7399b703afcd68942c
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-23 11:55:20 +0300

    [chore #159717827] register user blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 """parent file for the app"""
 from flask import Flask
-from .resources.questions import QUESTION
+from app.resources.questions import QUESTION
 from .resources.users import users
+from .resources.answers import ANSWERS
 from instance.config import app_config
 
 
@@ -11,6 +12,7 @@ def create_app(config_name):
     APP=Flask(__name__, instance_relative_config=True)
     APP.register_blueprint(QUESTION)
     APP.register_blueprint(users)
+    APP.register_blueprint(ANSWERS)
     APP.config.from_object(app_config[config_name])
     APP.config.from_pyfile('config.py')
     return APP
