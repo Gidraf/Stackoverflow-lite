dd86c266d434de049938a0b68da83061eed3d253
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:21:40 +0300

    [chore #159870393] fix user model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,59 +3,65 @@ from . import database_connection
 import psycopg2
 
 class Users(object):
-    """user table class"""
-    def __init__(self, database_connection=database_connection()):
-        """initialize the connection and cursor"""
-        self.cursor=database_connection.cursor()
-        self.database_connection=database_connection
+    """
+    user table class
+    """
 
-    def create_user_table(self):
-        "create users table"
+    def create_user_table(self, connection):
+        """
+        create users table
+        """
         sql="""CREATE TABLE IF NOT EXISTS users(
         userid SERIAL PRIMARY KEY UNIQUE NOT NULL,
         username VARCHAR(60) NOT NULL UNIQUE,
         useremail VARCHAR(120) NOT NULL UNIQUE,
         password VARCHAR(120) NOT NULL
         )"""
-        self.cursor.execute(sql)
-        self.database_connection.commit()
+        cursor = connection.cursor()
+        cursor.execute(sql)
 
-    def register_user(self, username,email,password):
-        "add user to the database in user table"
+    def register_user(self, username,email,password, cursor):
+        """
+        add user to the database in user table
+        """
         sql="""INSERT INTO users(username,useremail,password) VALUES(%s,%s,%s)"""
-        self.cursor.execute(sql,(username,email,password))
-        self.database_connection.commit()
+        cursor.execute(sql,(username,email,password))
+        return cursor
 
-    def update_user(self, userid, username, email, password):
-        "update user details in the database"
+    def update_user(self, userid, username, email, password, cursor):
+        """
+        update user details in the database
+        """
         sql="UPDATE users SET username=%s, email=%s, password=%s WHERE userid=%s"
-        self.cursor.execute(sql,(username,email,password,userid))
-        self.cursor.close
-        self.database_connection.commit()
-        self.database_connection.close()
+        cursor.execute(sql,(username,email,password,userid))
+        return cursor
 
-    def delete_user(self,userid):
-        "delete user by Id"
+    def delete_user(self,userid, cursor):
+        """
+        delete user by Id
+        """
         sql="DELETE FROM users WHERE userid = %s"
-        self.cursor.execute(sql,(userid))
-        self.database_connection.commit()
+        cursor.execute(sql,(userid))
 
-    def search_user_by_username(self,username):
-        "search user by username"
+    def search_user_by_username(self,username, cursor):
+        """
+        search user by username
+        """
         sql="SELECT * FROM users WHERE username=%s"
-        self.cursor.execute(sql,(username,))
-        user=self.cursor.fetchall()
-        return user
+        cursor.execute(sql,(username,))
+        return cursor
 
-    def search_user_by_email(self,useremail):
-        "search user by email"
+    def search_user_by_email(self,useremail, cursor):
+        """
+        search user by email
+        """
         sql = "SELECT * FROM users WHERE useremail=%s"
-        self.cursor.execute(sql,(useremail,))
-        user = self.cursor.fetchall()
-        return user
+        cursor.execute(sql,(useremail,))
+        return cursor
 
-    def clear_user_table(self):
-        "clear everything in user table"
-        sql="""DELETE FROM users"""
-        self.cursor.execute(sql)
-        self.database_connection.commit()
+    def clear_user_table(self,connection):
+        """
+        clear everything in user table
+        """
+        sql="""DROP TABLE users  CASCADE"""
+        connection.cursor.execute(sql)
