4e109ab9485dfcfa28cf8fb1647609d3c5c84740
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:16:35 +0300

    [chore #160041794] update table functionalities

--- app/models/answers_model.py
+++ app/models/answers_model.py
@@ -2,77 +2,91 @@
 from . import  database_connection
 
 class Answers(object):
-    """answer table class"""
+    """
+    answer table class
+    """
 
-    def __init__(self, database_connection=database_connection()):
-        """initialize the connection and cursor"""
-        self.cursor=database_connection.cursor()
-        self.database_connection=database_connection
-
-    def create_answer_table(self):
-        """create answer table"""
-        sql="""CREATE TABLE IF NOT EXISTS answers(
+    def create_answer_table(self,connection):
+        """
+        create answer table
+        """
+        sql="""
+        CREATE TABLE IF NOT EXISTS answers(
         answerid SERIAL PRIMARY KEY UNIQUE NOT NULL,
         answer_text TEXT NOT NULL,
         time_created TEXT NOT NULL,
         userid INTEGER NOT NULL,
         is_answer BOOLEAN,
-        votes BIGINT DEFAULT 0,
+        votes BIGINT,
         FOREIGN KEY (userid) REFERENCES users(userid) ON UPDATE CASCADE ON DELETE CASCADE,
         questionid INTEGER NOT NULL,
         FOREIGN KEY (questionid) REFERENCES questions(questionid) ON UPDATE CASCADE ON DELETE CASCADE
-        )"""
-        self.cursor.execute(sql)
-        self.database_connection.commit()
-
-    def add_answer(self, answer_text,time_created,userid, questionid):
-        """add answer to the database in user table"""
-        sql="""INSERT INTO answers(answer_text,time_created,userid, questionid) VALUES(%s,%s,%s,%s)
+        )
         """
-        self.cursor.execute(sql,(answer_text,time_created,userid, questionid))
-        self.database_connection.commit()
+        cursor = connection.cursor()
+        cursor.execute(sql)
 
-    def update_answer(self, answer_text, answerid):
-        "update answer details in the database"
+    def add_answer(self, answer_text,time_created,userid, questionid,vote,is_answer, cursor):
+        """
+        add answer to the database in user table
+        """
+        sql="""INSERT INTO answers(answer_text,time_created,userid, questionid,votes,is_answer) VALUES(%s,%s,%s,%s,%s,%s)
+        """
+        cursor.execute(sql,(answer_text,time_created,userid, questionid,vote,is_answer))
+        return cursor
+
+    def update_answer(self, answer_text, answerid, cursor):
+        """
+        update answer details in the database
+        """
         sql="UPDATE answers SET  answer_text=%s WHERE answerid=%s"
         self.cursor.execute(sql,(answer_text, answerid,))
-        self.database_connection.commit()
+        return cursor
 
-    def delete_answer(self,answerid):
-        "delete answer by id"
+    def delete_answer(self,answerid, cursor):
+        """
+        delete answer by id
+        """
         sql="DELETE FROM answers WHERE answers.answerid = %s"
-        self.cursor.execute(sql,(answerid,))
-        self.database_connection.commit()
-        return True
+        cursor.execute(sql,(answerid,))
+        return cursor
 
-    def search_answer_by_questionid(self,questionid):
-        "search answer by questionid"
+    def search_answer_by_questionid(self,questionid, cursor):
+        """
+        search answer by questionid
+        """
         sql="SELECT * FROM answers WHERE questionid = %s"
-        self.cursor.execute(sql,(questionid,))
-        answer=self.cursor.fetchall()
-        return answer
+        cursor.execute(sql,(questionid,))
+        return cursor
 
-    def down_vote_answer(self, answerid, vote):
-        """donvote answers"""
+    def down_vote_answer(self, answerid, vote, cursor):
+        """
+        donvote answers
+        """
         sql="UPDATE answers SET votes+=1 WHERE answerid =%s"
-        self.cursor.execute(sql,(vote,))
-        self.database_connection.commit()
+        cursor.execute(sql,(vote,))
+        return cursor
 
-    def mark_prefered(self, answerid, is_answer):
-        """mark as answer"""
+    def mark_prefered(self, answerid, is_answer, cursor):
+        """
+        mark as answer
+        """
         sql="UPDATE answers SET is_answer = %s WHERE answerid = %s"
-        self.cursor.execute(sql,(is_answer,))
-        self.database_connection.commit()
+        cursor.execute(sql,(is_answer,))
+        return cursor
 
-    def search_answer_by_user(self, userid):
-        """search answers user asked"""
+    def search_answer_by_user(self, userid, cursor):
+        """
+        search answers user asked
+        """
         sql="SELECT * FROM answers WHERE userid = %s"
-        self.cursor.execute(sql,(userid,))
-        answer=self.cursor.fetchall()
-        return answer
+        cursor.execute(sql,(userid,))
+        return cursor
 
-    def clear_answer_table(self):
-        "clear answer table"
+    def clear_answer_table(self, connection):
+        """
+        clear answer table
+        """
         sql="DELETE * FROM answers"
-        self.cursor.execute(sql)
-        self.connection.commmit()
+        cursor = connection.cursor()
+        cursor.execute(sql)
