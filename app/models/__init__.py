39ac1ba4f63d409e0e705812478600a55b1852b1
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:14:39 +0300

    [chore #159870393] fix database init to acccept both test and production

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,15 @@
 import psycopg2
 from instance.config import params
+from instance.config import test_params
 
-
-def database_connection():
-    """connect to database"""
+def database_connection(connection_type):
+    """
+    database connection
+    """
+    if connection_type == "testing":
+        connection=psycopg2.connect(**test_params)
+        connection.autocommit = True
+        return connection
     connection=psycopg2.connect(**params)
-
+    connection.autocommit = True
     return connection
