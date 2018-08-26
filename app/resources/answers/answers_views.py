32726f5808226487aa4d7d26f680d2cc2d2de635
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:22:10 +0300

    fix answer views

--- app/resources/answers/answers_views.py
+++ app/resources/answers/answers_views.py
@@ -10,6 +10,7 @@ from flask import abort
 from flask import make_response
 from app.models.answers_model import Answers
 from app.models.questions_model import Questions
+from app.models import database_connection
 from . import ANSWERS
 from flask_jwt_extended import jwt_required
 
@@ -27,35 +28,44 @@ def bad_request(error):
     return make_response(jsonify({"error":"the number or request you have entered is not accepted"}))
 
 @ANSWERS.route("/api/v1/answers/<int:questionid>", methods=["GET","POST"])
-@jwt_required
 def question_view(questionid):
     """show all answer of a question"""
     current_user = get_jwt_identity()
     try:
-        answer = answers.search_answer_by_questionid(questionid)
-        if answer:
-            quiz=queston.search_answer_by_questionid(questionid)
-            result={}
-            result["question"]=quiz[0]
-            result["answers"]=quiz[0]
-            return jsonify({"answers": result}),200
-        abort(404)
-    except (Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"error":str(error)})
+        connection=database_connection("development")
+        try:
+            cursor = answers.search_answer_by_questionid(questionid, connection.cursor())
+            answer = cursor.fetchall()
+            if answer:
+                quiz=queston.search_question_by_questionid(questionid)
+                result={}
+                result["question"]=quiz[0]
+                result["answers"]=answers
+                return jsonify({"answers": result}),200
+            abort(404)
+        except (Exception, psycopg2.DatabaseError) as error:
+            return jsonify({"error":str(error)}),500
+        connection.close()
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),500
 
 @ANSWERS.route('/api/v1/update_answer/<int:answerid>', methods=["PUT"])
 @jwt_required
 def update_answer(answerid):
     """update answer"""
     current_user = get_jwt_identity()
-    if not request.json or not "answer_text" in request.json:
-        abort(404)
-    answer_text = request.json["answer_text"]
     try:
-        answers.update_answer(answer_text,answe)
-        return jsonify({"info":"answer updated view questin to see changes made"})
-    except Exception as error:
-        return jsonify({"error": str(error)})
+        connection=database_connection("development")
+        if not request.json or not "answer_text" in request.json:
+            abort(404)
+        answer_text = request.json["answer_text"]
+        try:
+            answers.update_answer(answer_text,answerid,connection.cursor())
+            return jsonify({"info":"answer updated"}),200
+        except Exception as error:
+            return jsonify({"error": str(error)}),500
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),500
 
 @ANSWERS.route('/api/v1/delete_answer/<int:answerid>', methods=["DELETE"])
 @jwt_required
@@ -63,37 +73,49 @@ def delete_answer(answerid):
     '''delete answer'''
     current_user = get_jwt_identity()
     try:
-        answers.delete_answer(answerid)
-        return jsonify({"info":"deleted"})
-    except (Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"error":str(error)})
+        connection=database_connection("development")
+        try:
+            answers.delete_answer(answerid,connection.cursor())
+            return jsonify({"info":"deleted"}),200
+        except (Exception, psycopg2.DatabaseError) as error:
+            return jsonify({"error":str(error)}),500
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),500
 
 @ANSWERS.route('/api/v1/down_vote_answer/<int:answerid>', methods=["PUT"])
 @jwt_required
 def down_vote_answer(answerid):
     """update answer"""
     current_user = get_jwt_identity()
-    if not request.json or not "vote" in request.json:
-        abort(404)
-    vote=request.json["vote"]
-    if isinstance(vote, int):
-        try:
-            answers.vote_answer(answerid,vote)
-            return redirect(url_for("question_view"))
-        except Exception as error:
-            return jsonify({"error": str(error)})
-    abort(400)
+    try:
+        connection=database_connection("development")
+        if not request.json or not "vote" in request.json:
+            abort(404)
+        vote=request.json["vote"]
+        if isinstance(vote, int):
+            try:
+                answers.vote_answer(answerid,vote, connection.cursor())
+                return jsonify({"info":"voted"}),200
+            except Exception as error:
+                return jsonify({"error": str(error)}),500
+        abort(400)
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),500
 
 @ANSWERS.route('/api/v1/prefered_answer/<int:answerid>', methods=["GET"])
 @jwt_required
 def mark_prefered(answerid):
     """ mark answer as prefered"""
     current_user = get_jwt_identity()
-    if not request.json or not "vote" in request.json:
-        abort(400)
-    is_answer=request.json["is_answer"]
     try:
-        answers.mark_prefered(answerid, True)
-        return redirect (url_for("question_view"))
-    except (Exception,psycopg2.DatabaseError) as error:
-        return jsonify({"error": str(error)})
+        connection=database_connection("development")
+        if not request.json or not "vote" in request.json:
+            abort(400)
+        is_answer=request.json["is_answer"]
+        try:
+            answers.mark_prefered(answerid, True)
+            return redirect (url_for("question_view"))
+        except (Exception,psycopg2.DatabaseError) as error:
+            return jsonify({"error": str(error)})
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),200
