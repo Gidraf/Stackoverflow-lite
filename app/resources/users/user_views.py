060a7c00ce7414c57b7061f967419c5c815f4c95
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:22:29 +0300

    fix user views

--- app/resources/users/user_views.py
+++ app/resources/users/user_views.py
@@ -11,6 +11,7 @@ from flask_jwt_extended import (create_access_token,
 )
 from . import users
 from app.models.user_model import Users
+from app.models import database_connection
 user = Users()
 
 @users.errorhandler(404)
@@ -26,52 +27,67 @@ def bad_request(error):
 
 @users.route("/auth/register", methods=["POST"])
 def register():
-    "register user route"
-    if not request.json or not "useremail" in request.json:
-        abort(404)
-    if not "username" in request.json:
-        abort(404)
-    if not "password" in request.json:
-        abort(404)
-    name= request.json["username"]
-    email=request.json["useremail"]
-    password=request.json["password"]
     try:
-        user.create_user_table()
-        if user.search_user_by_email(email):
-            return jsonify({"info":"email in use"})
-        elif user.search_user_by_username(name):
-            return jsonify({"info": "username in use"})
-        user.register_user(name,email,password)
-        return jsonify({"info":"user registered"}),201
-    except (Exception, psycopg2.DatabaseError) as error:
-        return jsonify({"error":str(error)})
+        connection=database_connection("development")
+        "register user route"
+        if not request.json or not "useremail" in request.json:
+            abort(404)
+        if not "username" in request.json:
+            abort(404)
+        if not "password" in request.json:
+            abort(404)
+        name= request.json["username"]
+        email=request.json["useremail"]
+        password=request.json["password"]
+        try:
+            user.create_user_table(connection)
+            email_cursor = user.search_user_by_email(email,cursor=connection.cursor())
+            username_cursor = user.search_user_by_username(name,cursor=connection.cursor())
+            current_user_email=email_cursor.fetchall()
+            current_user_username=username_cursor.fetchall()
+            if current_user_email:
+                return jsonify({"info":"email in use"}),200
+            elif current_user_username:
+                return jsonify({"info": "username in use"}),200
+            user.register_user(name,email,password,cursor=connection.cursor())
+            return jsonify({"info":"user registered"}),201
+        except (Exception, psycopg2.DatabaseError) as error:
+            return jsonify({"error":str(error)}),500
+        connection.close
+    except Exception as e:
+        return jsonify({"error":"database connection error"}),500
 
 @users.route ("/auth/login", methods=["POST"])
 def login():
-    """login users to the database"""
-    if not request.json:
-        abort(400)
-    if not "username" in request.json:
-        abort(400)
-    if not "password" in request.json:
-        abort(400)
+    try:
+        connection=database_connection("development")
+        """login users to the database"""
+        if not request.json:
+            abort(400)
+        if not "username" in request.json:
+            abort(400)
+        if not "password" in request.json:
+            abort(400)
 
-    username=request.json["username"]
-    password=request.json["password"]
-    if not isinstance(username, (int,float)):
-        character=string.punctuation
-        if any(char in character  for char in username):
-            return jsonify({"error":"username can't have characters"})
-        try:
-            current_user =user.search_user_by_username(username)
-            if current_user:
-                set_password=current_user[0][3]
-                if str(password) == set_password:
-                    token=create_access_token(identity=username)
-                    return jsonify({"success":"your access token is","token":token}),200
-                return jsonify({"error":current_user[0]})#jsonify({"warning":"wrong password"})
-            return jsonify({"info":"no account found"})
-        except (Exception,psycopg2.DatabaseError) as error:
-            jsonify({"eror": error})
-    return "username must be a string"
+        username=request.json["username"]
+        password=request.json["password"]
+        if not isinstance(username, (int,float)):
+            character=string.punctuation
+            if any(char in character  for char in username):
+                return jsonify({"error":"username can't have characters"})
+            try:
+                cursor = user.search_user_by_username(username,cursor=connection.cursor())
+                current_user = cursor.fetchall()
+                if current_user:
+                    set_password=current_user[0][3]
+                    if str(password) == set_password:
+                        token=create_access_token(identity=username)
+                        return jsonify({"success":"your access token is","token":token}),200
+                    return jsonify({"error":"wrong password"}),400
+                return jsonify({"info":"no account found"}),404
+            except (Exception,psycopg2.DatabaseError) as error:
+                jsonify({"eror": error}),500
+        return jsonify({"error":"username must be a character"})
+        connection.close()
+    except Exception as e:
+        return ({"error": "database connection error"}),500
