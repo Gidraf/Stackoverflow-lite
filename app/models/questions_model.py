16e50cb55eb59bd8d42d57a51455d1bc5fd88aa2
Gidraf Orenja <orenjagidraf@gmail.com>
2018-08-26 05:21:03 +0300

    [chore #159870393] fix questions model

--- app/models/questions_model.py
+++ app/models/questions_model.py
@@ -3,12 +3,8 @@ from . import database_connection
 
 class Questions(object):
     """question table class"""
-    def __init__(self, database_connection=database_connection()):
-        """initialize the connection and cursor"""
-        self.cursor=database_connection.cursor()
-        self.database_connection=database_connection
 
-    def create_question_table(self):
+    def create_question_table(self,connection):
         "create question table"
         sql="""CREATE TABLE IF NOT EXISTS questions(
         questionid SERIAL PRIMARY KEY UNIQUE NOT NULL,
@@ -18,58 +14,53 @@ class Questions(object):
         userid INTEGER NOT NULL,
         FOREIGN KEY (userid) REFERENCES users(userid) ON UPDATE CASCADE ON DELETE CASCADE
         )"""
-        self.cursor.execute(sql)
-        self.database_connection.commit()
+        cursor = connection.cursor()
+        cursor.execute(sql)
 
-    def add_question(self, title, description, time_created,userid):
+    def add_question(self, title, description, time_created,userid, cursor):
         "add question to the database in user table"
         sql="""INSERT INTO questions(title,description,time_created,userid) VALUES(%s,%s,%s,%s)
         """
-        self.cursor.execute(sql,(title,description,time_created, userid))
-        self.database_connection.commit()
+        cursor.execute(sql,(title,description,time_created, userid))
+        return cursor
 
-    def update_question(self, title, description, questionid):
+    def update_question(self, title, description, questionid, cursor):
         "update question details in the database"
         sql="UPDATE questions SET title=%s, description=%s WHERE questionid=%s;"
-        self.cursor.execute(sql,(title,description,questionid))
-        self.database_connection.commit()
+        cursor.execute(sql,(title,description,questionid))
+        return cursor
 
-    def delete_question(self,questionid):
+    def delete_question(self,questionid,cursor):
         "delete question by id"
         sql="DELETE FROM questions WHERE questions.questionid = %s"
-        self.cursor.execute(sql,(questionid,))
-        self.database_connection.commit()
-        return True
+        cursor.execute(sql,(questionid,))
+        return cursor
 
-    def search_question_by_title(self,title):
+    def search_question_by_title(self,title,cursor):
         "search question by title"
         sql="SELECT * FROM questions WHERE title Like %s"
-        self.cursor.execute(sql,(title,))
-        question=self.cursor.fetchall()
-        return question
+        cursor.execute(sql,(title,))
+        return cursor
 
-    def search_question_by_questionid(self,questionid):
+    def search_question_by_questionid(self,questionid,cursor):
         "search question by id"
         sql="SELECT * FROM questions WHERE questionid = %s"
-        self.cursor.execute(sql,(questionid,))
-        question=self.cursor.fetchall()
-        return question
+        cursor.execute(sql,(questionid,))
+        return cursor
 
-    def search_question_by_user(self,userid):
+    def search_question_by_user(self,userid, cursor):
         sql="SELECT * FROM questions WHERE userid = %s"
-        self.cursor.execute(sql,(userid,))
-        question=self.cursor.fetchall()
-        return question
+        cursor.execute(sql,(userid,))
+        return cursor
 
-    def fetch_all_question(self):
+    def fetch_all_question(self, cursor):
         "fetchall questions"
         sql = "SELECT * FROM questions"
-        self.cursor.execute(sql)
-        questions = self.cursor.fetchall()
-        return questions
+        cursor.execute(sql)
+        return cursor
 
-    def clear_question_table(self):
+    def clear_question_table(self,connection):
         "clear user table"
-        sql="""DELETE FROM questions"""
-        self.cursor.execute(sql)
-        self.connection.commit()
+        sql="""DROP TABLE questions CASCADE"""
+        cursor = connection.cursor()
+        cursor.execute(sql)
