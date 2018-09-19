8da90adad41ced0b67ebecf077aad1fcd1db3759
Gidraf Orenja <orenjagidraf@gmail.com>
2018-09-19 17:56:35 +0300

    Update: ad search user questions

--- app/resources/users/user_views.py
+++ app/resources/users/user_views.py
@@ -10,12 +10,16 @@ from flask import request
 from validate_email import validate_email
 import string
 from flask_jwt_extended import (create_access_token,
-    get_jwt_identity)
+    get_jwt_identity, jwt_required)
 from . import users
 from app.models.user_model import Users
+from app.models.questions_model import Questions
+from app.models.answers_model import Answers
 from app.models import database_connection
 
 user = Users()
+question = Questions()
+answer = Answers()
 
 @users.route("/auth/register", methods=["POST"])
 def register():
@@ -25,8 +29,6 @@ def register():
     """
     try:
         connection=database_connection("development")
-        if not request.json:
-             return jsonify({"error":"response body can't be empty"}),400
         if not "username" in request.json:
              return jsonify({"error":"'username' key not found"}),400
         if not "password" in request.json:
@@ -41,17 +43,20 @@ def register():
             character=string.punctuation
             if any(char in character  for char in username) or not  username.strip():
                 return jsonify({"error":"invalid username"}),400
-            email_cursor = user.search_user_by_email(email,cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
-            username_cursor = user.search_user_by_username(username,cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+            email_cursor = user.search_user_by_email(email,\
+            cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+            username_cursor = user.search_user_by_username(username,\
+            cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
             current_user_email=email_cursor.fetchone()
             current_user_username=username_cursor.fetchone()
             if current_user_email:
                 return jsonify({"warning":"email already in use"}),401
             elif current_user_username:
                 return jsonify({"warning": "username already in use"}),401
-            user.register_user(username,email,password,cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
-            return jsonify({"success":"user registered"}),201
-        return jsonify({"error":"invalid username or email"}),400
+            user.register_user(username,email,password,\
+            cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+            return jsonify({"success":"registered"}),201
+        return jsonify({"error":"invalid password"}),400
         connection.close
     except (Exception, psycopg2.DatabaseError) as e:
         return jsonify({"error":str(e)}),400
@@ -63,8 +68,6 @@ def login():
     """
     try:
         connection=database_connection("development")
-        if not request.json:
-             return jsonify({"error":"response body can't be empty"}),400
         if not "username" in request.json:
              return jsonify({"error":"'username' key not found"}),400
         if not "password" in request.json:
@@ -76,7 +79,8 @@ def login():
             if any(char in character  for char in username) or not\
              username.strip() or not password.strip():
                 return jsonify({"error":"invalid username or password"}),400
-            cursor = user.search_user_by_username(username,cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+            cursor = user.search_user_by_username(username,\
+            cursor=connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
             current_user = cursor.fetchone()
             if current_user:
                 set_password=current_user["password"]
@@ -84,8 +88,41 @@ def login():
                     expires = datetime.timedelta(days=1)
                     token=create_access_token(identity=current_user, expires_delta=expires)
                     return jsonify({"success":"you have been logged in","token":token}),200
-                return jsonify({"error":set_password+"!=" + password}),401
-            return jsonify({"info":"no account found"}),404
+                return jsonify({"error":"Wrong password"}),401
+            return jsonify({"error":"no account found"}),404
         connection.close()
     except Exception as e:
         return jsonify({"error": str(e)}),400
+
+@users.route("/api/v1/user/questions/<int:userid>", methods=["GET"])
+@jwt_required
+def get_user_questions(userid):
+    """
+    fetch user questions from database
+    """
+    connection = database_connection("development")
+    try:
+        cursor = question.search_question_userid(userid, \
+        connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+        user_questions = cursor.fetchall()
+        if user_questions:
+            result = []
+            for q, i in enumerate(user_questions, start = 0):
+                question_container = {}
+                question_owners = {}
+                answers_length = {}
+                questions_asked = {}
+                user_cursor = user.search_user_by_id(userid,\
+                connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+                user_name = user_cursor.fetchone()
+                answer_cursor = answer.search_answer_by_questionid(user_questions[q]["questionid"],\
+                connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor))
+                question_answer = answer_cursor.fetchall()
+                question_container["user"] =  user_name["username"]
+                question_container["answers"] = len(question_answer)
+                question_container ["questions"] = i
+                result.append(question_container)
+            return jsonify ({"result":result}),200
+        return ({"error":"no question found"}),404
+    except (Exception, psycopg2.DatabaseError) as error:
+        return jsonify ({"error":str(error)}),400
